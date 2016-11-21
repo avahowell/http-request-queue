14bd19382b19bddbea9369139270afb32ef131d1
Johnathan Howell <me@johnathanhowell.com>
2016-11-21 17:21:22 -0500

    increase version to 0.1.0

--- src/rqueue.js
+++ src/rqueue.js
@@ -1,7 +1,10 @@
 import req from 'request'
 
+// retryPollSpeed determines the interval at which enqueued requests are
+// retried.
 const retryPollSpeed = 200
 
+// reqpromise promisifies request.js request calls.
 const reqpromise = (opts) => new Promise((resolve, reject) => {
 	req(opts, (err, response, body) => {
 		if (err) {
@@ -13,6 +16,8 @@ const reqpromise = (opts) => new Promise((resolve, reject) => {
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
+// queue returns a http-request-queue object with a limit of concurrent
+// requests determined by `size`.
 const queue = (size = 10) => {
 	let inflight = 0
 	const request = async (opts) => {
